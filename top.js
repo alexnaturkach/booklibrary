6b31f932490acbf95eb10e185adb56e36a4710f4
Oleksandr Naturkach <oleksandrnaturkach@gmail.com>
2020-09-25 16:44:20 +0300

    skeleton + logic

--- top.js
+++ top.js
@@ -1,10 +1,57 @@
-function Book(title, author, pages, status){
+let myLibrary = [];
+const inProgressBooks = document.querySelector('.inProgress');
+const toReadBooks = document.querySelector('.toRead');
+const finishedBooks = document.querySelector('.finished');
+
+
+showBooks();
+
+function Book(title, author, onPage, totalPages) {
   this.title = title;
   this.author = author;
-  this.pages = pages;
-  this.status = status;
-  this.info = function(){
-
-    return this.title + ', ' + this.author + ', ' + 'Number of pages:' + this.pages + ', ' + 'Read status:' + this.status;
+  this.onPage = onPage;
+  this.totalPages = totalPages;
+  this.info = function() {
+    return this;
   }
 }
+
+
+function addBookToLibrary() {
+  let a = document.getElementById('bookTitle').value;
+  let b = document.getElementById('authorName').value;
+  let c = document.getElementById('onPage').value;
+  let d = document.getElementById('numOfPages').value;
+  myLibrary.push(new Book(a, b, c, d));
+  showBooks();
+}
+
+
+function showBooks() {
+  inProgressBooks.innerHTML = '';
+  finishedBooks.innerHTML = '';
+  for (i = 0; i < myLibrary.length; i++) {
+    if (Number(myLibrary[i].onPage) < Number(myLibrary[i].totalPages)) {
+      let newP = document.createElement('p');
+      let pText = document.createTextNode(myLibrary[i].title + ' by ' + myLibrary[i].author);
+      newP.appendChild(pText);
+      let xMarker = document.createElement('span');
+      xMarker.innerHTML = 'Delete';
+      xMarker.classList.add('delete');
+      newP.appendChild(xMarker);
+      newP.classList.add('books');
+      inProgressBooks.prepend(newP);
+    }
+    else if (Number(myLibrary[i].onPage) === Number(myLibrary[i].totalPages)){
+      let newP = document.createElement('p');
+      let pText = document.createTextNode(myLibrary[i].title + ' by ' + myLibrary[i].author);
+      newP.appendChild(pText);
+      let xMarker = document.createElement('span');
+      xMarker.innerHTML = 'Delete';
+      xMarker.classList.add('delete');
+      newP.appendChild(xMarker);
+      newP.classList.add('books');
+      finishedBooks.prepend(newP);
+    }
+  }
+};
